e4873ab67a45d0c1c291e12b0d585c27a76ea1e5
quesnok <quesnok@afkdev.de>
2025-03-23 10:20:28 +0100

    add: cucumber lsp, markdown viewer

--- nvim/lua/quesnok/packer.lua
+++ nvim/lua/quesnok/packer.lua
@@ -60,9 +60,25 @@ use {
             require("typescript-tools").setup({})
         end,
 }
+-- cucumber
+use {"tpope/vim-cucumber",
+    ft = { "cucumber", "feature" }
+}
 
   -- dark theme --
   use {"olimorris/onedarkpro.nvim"}
 
+  -- render markdown
+  use({
+    'MeanderingProgrammer/render-markdown.nvim',
+    after = { 'nvim-treesitter' },
+    -- requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+        require('render-markdown').setup({})
+    end,
+})
+use {'MunifTanjim/eslint.nvim'}
 end)
 
