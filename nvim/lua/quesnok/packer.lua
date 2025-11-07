e2169759f280076a95545abd591fac87258e98fe
quesnok <quesnok@afkdev.de>
2025-11-07 09:14:42 +0100

    add gitignore - ignore packer_compiled

--- nvim/lua/quesnok/packer.lua
+++ nvim/lua/quesnok/packer.lua
@@ -5,9 +5,6 @@ return require('packer').startup(function(use)
     use({
         "L3MON4D3/LuaSnip",
         -- follow latest release.
-        tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!:).
-        run = "make install_jsregexp"
     })
   -- fuzzy finder --
   use {'nvim-telescope/telescope.nvim',
