eac519839fac267b74f7b59509dbad2c19a8cb6d
quesnok <quesnok@afkdev.de>
2025-11-07 09:12:14 +0100

    feat(keymaps): add split navigation shortcuts and window helpers

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -8,6 +8,11 @@ vim.keymap.set('n', '<leader>gr', function()
 vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>q', ':bd<CR>', {noremap = true})
 
-
-
-
+-- Remap "move to left split" to Ctrl+h
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+-- Remap "move to right split" to Ctrl+l
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+-- Remap "move to top split" to Ctrl+k
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+-- Remap "move to bottom split" to Ctrl+j
+vim.keymap.set('n', '<C-j>', '<C-w>j')
--- nvim/lua/quesnok/remap.lua
+++ nvim/lua/quesnok/remap.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>c", function()
   require("treesitter-context").go_to_context(vim.v.count1)
 end, { silent = true })
+
+
+vim.keymap.set('n', '<C-1>', ':wincmd p<CR>')
+vim.keymap.set('n', '<C-2>', ':wincmd n<CR>')
