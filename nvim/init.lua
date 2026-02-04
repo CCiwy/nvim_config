e0d07db547246cae80039ded98369b2ba8bca51e
quesnok <quesnok@afkdev.de>
2026-02-04 11:30:53 +0100

    feat: add odin lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,8 +87,37 @@ local servers = {
   'cucumber_language_server',
   'shopify_theme_ls',
   'ruff',
-  'ocamllsp'
+  'ocamllsp',
+  'ols'
 }
+
+lsp.config('ols', {
+  cmd = { "odinls" },
+  init_options = {
+    checker_args = "-strict-style",
+    collections = {
+      { name = "shared", path = vim.fn.expand("$HOME/odin-lib") },
+    },
+    enable_format = true,
+    enable_hover = true,
+    enable_document_symbols = true,
+    enable_semantic_tokens = true,
+    enable_inlay_hints = true,
+    formatter = {
+      style = "stroustrup",  -- Brace style
+    },
+  },
+})
+
+-- Format on save for Odin files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.odin",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
+
 
 lsp.config('*', {
   capabilities = capabilities,
