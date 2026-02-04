7922fef886575766a2b2b9d169e52b5b3a9466c4
quesnok <quesnok@afkdev.de>
2026-02-04 11:29:24 +0100

    feat: make active line number more distinctivly visible
    higher contrast color + bold

--- nvim/lua/quesnok/set.lua
+++ nvim/lua/quesnok/set.lua
@@ -13,3 +13,14 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 -- enable copy to clippboard
 vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+
+
+vim.api.nvim_set_hl(0, "CursorLineNr", {
+  fg = "#E6C384",
+  bold = true,
+})
+
+vim.api.nvim_set_hl(0, "CursorLine", {
+  bg = "#2A2A2A",
+})
+
