744cf4dec9494b317425ec047a48026ea52e7578
quesnok <quesnok@afkdev.de>
2026-02-05 12:19:42 +0100

    refactor: move non-telescope remaps to remap.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,18 +5,8 @@ vim.keymap.set('n', '<leader>gr', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end
 )
+
 vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>q', ':bd<CR>', {noremap = true})
-
--- Remap "move to left split" to Ctrl+h
-vim.keymap.set('n', '<C-h>', '<C-w>h')
--- Remap "move to right split" to Ctrl+l
-vim.keymap.set('n', '<C-l>', '<C-w>l')
--- Remap "move to top split" to Ctrl+k
-vim.keymap.set('n', '<C-k>', '<C-w>k')
--- Remap "move to bottom split" to Ctrl+j
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-
 
 -- telescope layout
 require('telescope').setup({
--- nvim/lua/quesnok/remap.lua
+++ nvim/lua/quesnok/remap.lua
@@ -7,10 +7,18 @@ end, { silent = true })
 vim.keymap.set('n', '<C-1>', ':wincmd p<CR>')
 vim.keymap.set('n', '<C-2>', ':wincmd n<CR>')
 
+vim.keymap.set('n', '<leader>q', ':bd<CR>', {noremap = true})
+
+-- move to splits with control + vim-motion
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+
 
 vim.keymap.set("n", "<F5>", function()
-  vim.cmd("silent make")
-  if #vim.fn.getqflist() > 0 then
-    vim.cmd("copen")
-  end
+    vim.cmd("silent make")
+    if #vim.fn.getqflist() > 0 then
+        vim.cmd("copen")
+    end
 end, { desc = "Build and open quickfix" })
