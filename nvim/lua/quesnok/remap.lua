456aa7de73fb8aeb1a79fd2ebb2024b689be8511
quesnok <quesnok@afkdev.de>
2026-02-05 11:59:40 +0100

    feat: add keybind for make (build and open quickfix)

--- nvim/lua/quesnok/remap.lua
+++ nvim/lua/quesnok/remap.lua
@@ -6,3 +6,11 @@ end, { silent = true })
 
 vim.keymap.set('n', '<C-1>', ':wincmd p<CR>')
 vim.keymap.set('n', '<C-2>', ':wincmd n<CR>')
+
+
+vim.keymap.set("n", "<F5>", function()
+  vim.cmd("silent make")
+  if #vim.fn.getqflist() > 0 then
+    vim.cmd("copen")
+  end
+end, { desc = "Build and open quickfix" })
